dc596ea7678b7956ff489c146cf4f581ae21c0cc
Chaitanya Shah <chashah4@publicisgroupe.net>
2019-08-19 17:10:17 +0530

    Test

---
+++ Week_5_Jenkins/src/test/java/com/example/demo/HelloTest.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+	@Test
+	public void test() {
+		int a=2+2;
+		assertEquals(4, a);
+	}
+
+}
